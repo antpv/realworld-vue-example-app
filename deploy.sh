b625d8de1e85e7e14cd78fa117b3d28b2107bf29
Antipov <antpv99@gmail.com>
2019-12-26 12:49:58 +0200

    Add deploy config

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/realworld-vue-example-app/' : '/',
+
   pluginOptions: {
     i18n: {
       locale: 'ru',
